1449a10fa34082e246b06945d4df78199657588c
knoff <nixos-git@knoc.one>
2023-12-02 14:50:38 +0100

    remoteInstall script works, fixed wifi on the lapt

--- remoteInstall.sh
+++ remoteInstall.sh
@@ -1,30 +1,85 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p bash
+#! nix-shell -i bash -p bash jq
 
 
 
 set -euo pipefail
 
-# get the configs from the flake
-nix flake show | grep -E "nixos-config|nixpkgs-overlays"
+function selectConfig() {
+
+    configs=$(nix flake show --json | jq -r '.nixosConfigurations')
+    # get the names of the configs
+    names=$(echo $configs | jq -r 'keys[]')
+
+
+
+    # select the config
+    i=0
+    for name in $names; do
+        echo "$i: $name"
+        i=$((i+1))
+    done
+
+    read -p "Select the config: " num
+
+    # check if the input is valid
+    if [[ ! $i =~ ^[0-9]+$ ]]; then
+        echo "Invalid input"
+        exit 1
+    fi
+
+    # get the name of the config
+    # bash array, num is the index
+    i=0
+    for name in $names; do
+        if [[ $i == $num ]]; then
+            config=$name
+            break
+        fi
+        i=$((i+1))
+    done
+
+}
 
 
 
 
-# ip address
-read -p "Enter the ip address: " ip
-
-# check if the ip address is valid
-if [[ ! $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    echo "Invalid ip address"
-    exit 1
-fi
-
-# check if the ip address is reachable
-if ! ping -c 1 $ip &> /dev/null; then
-    echo "Ip address not reachable"
-    exit 1
-fi
+function selectComputer() {
+    # use nmap to scan the network
+    # get the ip addresses of the devices
+    # first get the ip structure of the network
+    # should output something like 192.168.XXX.0/24
+#    ip=$(ip route | grep default | awk '{print $3}')
+#    ip=${ip%.*}.0/24
+#
+#    nmap -sn $ip
 
 
-nix run github:nix-community/nixos-anywhere -- --flake .#host root@$ip
+    # nix-shell -p nmap --run "nmap -sn 192.168.16.0/24"
+    # sho
+
+#    devices=$(nmap -sn )
+
+    # ip address
+    read -p "Enter the ip address: " ip
+
+    # check if the ip address is valid
+    if [[ ! $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        echo "Invalid ip address"
+        exit 1
+    fi
+
+    # check if the ip address is reachable
+    if ! ping -c 1 $ip &> /dev/null; then
+        echo "Ip address not reachable"
+        exit 1
+    fi
+    echo $ip
+}
+
+
+# get the config
+selectConfig
+ip=$(selectComputer)
+
+nix run github:nix-community/nixos-anywhere -- --flake .#$config root@$ip
--- systems/laptop.nix
+++ systems/laptop.nix
@@ -48,7 +48,7 @@
 
   # networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
