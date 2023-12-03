e4fe6f9a9c6cb057ab7b322faeaf19501b76b60b
knoff <nixos-git@knoc.one>
2023-12-03 01:15:53 +0100

    added home-manager bound to rebuild

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
     };
 
     # Home Manager (for managing user environments using Nix)
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # NixOS hardware-specific configurations
@@ -60,10 +60,27 @@
       nixosConfigurations = {
         laptop = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
+          home-manager.url = "github:nix-community/home-manager/release-23.05";
+          home-manager.inputs.nixpkgs.follows = "nixpkgs";
           modules = [
             ./systems/laptop.nix
             disko.nixosModules.disko
             #{ disko.devices.disk.vdb.device = "/dev/sda"; }
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.knoff = import ./home/knoff.nix;
+              home-manager.extraSpecialArgs = {
+                inherit inputs;
+                inherit outputs;
+                inherit system;
+                #inherit nix-colors; # Shold substitute this
+              };
+
+              # Optionally, use home-manager.extraSpecialArgs to pass
+              # arguments to home.nix
+            }
           ];
         };
         #desktop = nixpkgs.lib.nixosSystem {
