90b846d356eefafbac92444f8a7f3bcee2a083cc
knoff <nixos-git@knoc.one>
2023-12-04 00:04:59 +0100

    small changes, working firefox

--- home/programs/browser/profiles/main/default.nix
+++ home/programs/browser/profiles/main/default.nix
@@ -1,7 +1,7 @@
 { theme, pkgs, config, inputs, lib, ... }:
 let
   # unsigned int
-  id = 1;
+  id = 0;
 
   # computers name
   profileName = "main";
@@ -65,7 +65,7 @@ in
 
         # Appearance/functionality
         new-tab-override
-        adsum-notabs
+        sidebery
 
         smart-referer
         darkreader
@@ -109,202 +109,203 @@ in
       };
 
       # @import "./edge-mimicry/edge-mimicry/sidebar-mods.css";
-      userChrome = ''
-        @import "./firefox-csshacks/chrome/autohide_sidebar.css";
+      userChrome =
+        ''
+          @import "./firefox-csshacks/chrome/autohide_sidebar.css";
 
-        /* override sidebar options */
-        /* https://pastebin.com/KFHjwR4d */
-        #sidebar-box{
-          --uc-autohide-sidebar-delay: 100ms;
-          --uc-autohide-transition-duration: 115ms;
-          --uc-sidebar-width: 40px;
-        }
+          /* override sidebar options */
+          /* https://pastebin.com/KFHjwR4d */
+          #sidebar-box{
+            --uc-autohide-sidebar-delay: 100ms;
+            --uc-autohide-transition-duration: 115ms;
+            --uc-sidebar-width: 40px;
+          }
 
 
-        .tabbrowser-tabpanels {
+          .tabbrowser-tabpanels {
+              background-color: #${theme.base02} !important;
+          }
+          browser {
+              background-color: #${theme.base02} !important;
+          }
+
+          #bookmarksPanel, #history-panel {
+            background-color: #${theme.base01} !important;
+          }
+          #sidebar-header,#sidebar-search-container,#bookmarks-view-children,#historyTree {
+            color: #${theme.base01} !important;
+            background-color: #${theme.base01} !important;
+            -moz-appearance:none!important;
+            border-color:transparent !important;
+          }
+
+          #sidebar-box{
+            --sidebar-transition-delay: 100ms;
+            --sidebar-width: 48px;
+            --sidebar-hover-width: calc(calc(calc(var(--sidebar-width) - 0.65em) * 10) + 0.65em);
+            --autohide-sidebar-delay: 100ms; /* Delay before hiding the sidebar */
+            --sidebar-background-color: #${theme.base02} !important;
+          }
+
+          .sidebar-panel #search-box{
+            background-color: #${theme.base03} !important;
+            color: #${theme.base06} !important;
+          }
+
+          #sidebar,
+          #sidebar-header {
             background-color: #${theme.base02} !important;
-        }
-        browser {
+            border-bottom: none !important;
+            background-image: var(--lwt-additional-images);
+            background-position: auto;
+            background-size: auto;
+            background-repeat: no-repeat;
+          }
+
+          #browser {
+            --sidebar-border-color: #${theme.base01} !important;
+          }
+          #sidebar-header::before {
             background-color: #${theme.base02} !important;
-        }
+          }
 
-        #bookmarksPanel, #history-panel {
-          background-color: #${theme.base01} !important;
-        }
-        #sidebar-header,#sidebar-search-container,#bookmarks-view-children,#historyTree {
-          color: #${theme.base01} !important;
-          background-color: #${theme.base01} !important;
-          -moz-appearance:none!important;
-          border-color:transparent !important;
-        }
+          #sidebar-header::after{
+            background-color: #${theme.base02} !important;
+          }
 
-        #sidebar-box{
-          --sidebar-transition-delay: 100ms;
-          --sidebar-width: 48px;
-          --sidebar-hover-width: calc(calc(calc(var(--sidebar-width) - 0.65em) * 10) + 0.65em);
-          --autohide-sidebar-delay: 100ms; /* Delay before hiding the sidebar */
-          --sidebar-background-color: #${theme.base02} !important;
-        }
+          /* hides the native tabs */
+          #TabsToolbar {
+            visibility: collapse;
+          }
 
-        .sidebar-panel #search-box{
-          background-color: #${theme.base03} !important;
-          color: #${theme.base06} !important;
-        }
+          /* Hide window controls */
+          .titlebar-buttonbox-container{
+            display:none
+          }
+          .titlebar-spacer[type="post-tabs"]{
+            display:none
+          }
 
-        #sidebar,
-        #sidebar-header {
-          background-color: #${theme.base02} !important;
-          border-bottom: none !important;
-          background-image: var(--lwt-additional-images);
-          background-position: auto;
-          background-size: auto;
-          background-repeat: no-repeat;
-        }
+          /* Color Configs */
+          :root{
+            -moz-border-radius: 1em;
+            /* Popup panels */
+            --arrowpanel-background: #${theme.base01} !important;
+            --arrowpanel-border-color: #${theme.base00} !important;
+            --arrowpanel-color: #${theme.base06} !important;
+            --arrowpanel-dimmed: #${theme.base05} !important;
 
-        #browser {
-          --sidebar-border-color: #${theme.base01} !important;
-        }
-        #sidebar-header::before {
-          background-color: #${theme.base02} !important;
-        }
+            /* window and toolbar background */
+            --lwt-accent-color: #${theme.base01} !important;
+            --lwt-accent-color-inactive: #${theme.base00} !important;
+            --toolbar-bgcolor: #${theme.base01} !important;
 
-        #sidebar-header::after{
-          background-color: #${theme.base02} !important;
-        }
+            /* tabs with system theme - text is not controlled by variable */
+            --tab-selected-bgcolor: #${theme.base02} !important;
 
-        /* hides the native tabs */
-        #TabsToolbar {
-          visibility: collapse;
-        }
+            /* tabs with any other theme */
+            --lwt-text-color: #${theme.base05} !important;
+            --lwt-selected-tab-background-color: #${theme.base02} !important;
 
-        /* Hide window controls */
-        .titlebar-buttonbox-container{
-          display:none
-        }
-        .titlebar-spacer[type="post-tabs"]{
-          display:none
-        }
+            /* toolbar area */
+            --toolbarbutton-icon-fill: #${theme.base06} !important;
+            --lwt-toolbarbutton-hover-background: #${theme.base06} !important;
+            --lwt-toolbarbutton-active-background: #${theme.base05} !important;
 
-        /* Color Configs */
-        :root{
+            /* urlbar */
+            --toolbar-field-border-color: #${theme.base04} !important;
+            --toolbar-field-focus-border-color: #${theme.base05} !important;
+            --urlbar-popup-url-color: #${theme.base06} !important;
+
+            /* urlbar Firefox < 92 */
+            --lwt-toolbar-field-background-color: #${theme.base02} !important;
+            --lwt-toolbar-field-focus: #${theme.base07} !important;
+            --lwt-toolbar-field-color: #${theme.base06} !important;
+            --lwt-toolbar-field-focus-color: #${theme.base07} !important;
+
+            /* urlbar Firefox 92+ */
+            --toolbar-field-background-color: #${theme.base02} !important;
+            --toolbar-field-focus-background-color: #${theme.base03} !important;
+            --toolbar-field-color: #${theme.base06} !important;
+            --toolbar-field-focus-color: #${theme.base07} !important;
+
+            /* sidebar - note the sidebar-box rule for the header-area */
+            --lwt-sidebar-background-color: #${theme.base02} !important;
+            --lwt-sidebar-text-color: #${theme.base06} !important;
+          }
+
+          /* line between nav-bar and tabs toolbar,
+          also fallback color for border around selected tab */
+          #navigator-toolbox{ --lwt-tabs-border-color: #${theme.base02} !important; }
+          /* Line above tabs */
+          #tabbrowser-tabs{ --lwt-tab-line-color: #${theme.base05} !important; }
+          /* the header-area of sidebar needs this to work */
+          #sidebar-box{ --sidebar-background-color: #${theme.base00} !important; }
+
+          /* This changes the color of the loading page */
+          #tabbrowser-tabpanels,
+          #webextpanels-window,
+          #webext-panels-stack,
+          #webext-panels-browser {
+            background: #${theme.base02} !important;
+          }
+
+
+          /* Some variables for quick configuration - play with numbers to find a perfect match for your setup */
+          :root {
+              --sidebar-width: 0vw;
+              --panel-width: 100vw;
+              --panel-hide-offset: -30px;
+              --opacity-when-hidden: 0.0;
+          }
+
+
+          /* Auto-hide address bar */
+          #navigator-toolbox{
+            position: fixed !important;
+            /* Comment out following line to get 'slide-page-down' reveal, like in F11 fullscreen mode */
+            display: block;
+            transition: margin-top 82ms 33ms linear, opacity 82ms 33ms linear !important;
+            z-index: 1;
+            opacity: 1;
+            /* Spacing on the left for sidebar */
+            margin-left: var(--sidebar-width);
+            /* Disabled the borders, as the bottom one seemed to have unwanted top padding sometimes */
+            border: none !important;
+          }
+
+          #navigator-toolbox,
+          #navigator-toolbox > *{
+            /* Reduced width for panel in order to not overflow the screen on the right side */
+             width:  var(--panel-width);
+          }
+
+          /* if the cursor is at the top 30px of the screen, show the toolbar */
+          /* and if the cursor is at the left half of the screen show the toolbar */
+          #navigator-toolbox:not(:focus-within):not(:hover){
+            margin-top: var(--panel-hide-offset);
+            /* Hide the toolbar when not hovered */
+            opacity: var(--opacity-when-hidden);
+          }
+
+
+          /* Disable auto-hiding when in 'customize' mode */
+          :root[customizing] #navigator-toolbox{
+            position: relative !important;
+            opacity: 1 !important;
+            margin-top: 0px;
+          }
+
+
+
+          :root[tabsintitlebar] {
+            appearance: -moz-win-glass!important;
+          }
+
+
           -moz-border-radius: 1em;
-          /* Popup panels */
-          --arrowpanel-background: #${theme.base01} !important;
-          --arrowpanel-border-color: #${theme.base00} !important;
-          --arrowpanel-color: #${theme.base06} !important;
-          --arrowpanel-dimmed: #${theme.base05} !important;
 
-          /* window and toolbar background */
-          --lwt-accent-color: #${theme.base01} !important;
-          --lwt-accent-color-inactive: #${theme.base00} !important;
-          --toolbar-bgcolor: #${theme.base01} !important;
-
-          /* tabs with system theme - text is not controlled by variable */
-          --tab-selected-bgcolor: #${theme.base02} !important;
-
-          /* tabs with any other theme */
-          --lwt-text-color: #${theme.base05} !important;
-          --lwt-selected-tab-background-color: #${theme.base02} !important;
-
-          /* toolbar area */
-          --toolbarbutton-icon-fill: #${theme.base06} !important;
-          --lwt-toolbarbutton-hover-background: #${theme.base06} !important;
-          --lwt-toolbarbutton-active-background: #${theme.base05} !important;
-
-          /* urlbar */
-          --toolbar-field-border-color: #${theme.base04} !important;
-          --toolbar-field-focus-border-color: #${theme.base05} !important;
-          --urlbar-popup-url-color: #${theme.base06} !important;
-
-          /* urlbar Firefox < 92 */
-          --lwt-toolbar-field-background-color: #${theme.base02} !important;
-          --lwt-toolbar-field-focus: #${theme.base07} !important;
-          --lwt-toolbar-field-color: #${theme.base06} !important;
-          --lwt-toolbar-field-focus-color: #${theme.base07} !important;
-
-          /* urlbar Firefox 92+ */
-          --toolbar-field-background-color: #${theme.base02} !important;
-          --toolbar-field-focus-background-color: #${theme.base03} !important;
-          --toolbar-field-color: #${theme.base06} !important;
-          --toolbar-field-focus-color: #${theme.base07} !important;
-
-          /* sidebar - note the sidebar-box rule for the header-area */
-          --lwt-sidebar-background-color: #${theme.base02} !important;
-          --lwt-sidebar-text-color: #${theme.base06} !important;
-        }
-
-        /* line between nav-bar and tabs toolbar,
-        also fallback color for border around selected tab */
-        #navigator-toolbox{ --lwt-tabs-border-color: #${theme.base02} !important; }
-        /* Line above tabs */
-        #tabbrowser-tabs{ --lwt-tab-line-color: #${theme.base05} !important; }
-        /* the header-area of sidebar needs this to work */
-        #sidebar-box{ --sidebar-background-color: #${theme.base00} !important; }
-
-        /* This changes the color of the loading page */
-        #tabbrowser-tabpanels,
-        #webextpanels-window,
-        #webext-panels-stack,
-        #webext-panels-browser {
-          background: #${theme.base02} !important;
-        }
-
-
-        /* Some variables for quick configuration - play with numbers to find a perfect match for your setup */
-        :root {
-            --sidebar-width: 0vw;
-            --panel-width: 100vw;
-            --panel-hide-offset: -30px;
-            --opacity-when-hidden: 0.0;
-        }
-
-
-        /* Auto-hide address bar */
-        #navigator-toolbox{
-          position: fixed !important;
-          /* Comment out following line to get 'slide-page-down' reveal, like in F11 fullscreen mode */
-          display: block;
-          transition: margin-top 82ms 33ms linear, opacity 82ms 33ms linear !important;
-          z-index: 1;
-          opacity: 1;
-          /* Spacing on the left for sidebar */
-          margin-left: var(--sidebar-width);
-          /* Disabled the borders, as the bottom one seemed to have unwanted top padding sometimes */
-          border: none !important;
-        }
-
-        #navigator-toolbox,
-        #navigator-toolbox > *{
-          /* Reduced width for panel in order to not overflow the screen on the right side */
-           width:  var(--panel-width);
-        }
-
-        /* if the cursor is at the top 30px of the screen, show the toolbar */
-        /* and if the cursor is at the left half of the screen show the toolbar */
-        #navigator-toolbox:not(:focus-within):not(:hover){
-          margin-top: var(--panel-hide-offset);
-          /* Hide the toolbar when not hovered */
-          opacity: var(--opacity-when-hidden);
-        }
-
-
-        /* Disable auto-hiding when in 'customize' mode */
-        :root[customizing] #navigator-toolbox{
-          position: relative !important;
-          opacity: 1 !important;
-          margin-top: 0px;
-        }
-
-
-
-        :root[tabsintitlebar] {
-          appearance: -moz-win-glass!important;
-        }
-
-
-        -moz-border-radius: 1em;
-
-      '';
+        '';
       userContent = ''
         .tabbrowser-tabbox {
             background-color: #${theme.base02} !important;
