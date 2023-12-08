e5ef320da47abf962c8f5537d7d70a2010133821
knoff <nixos-git@knoc.one>
2023-12-08 20:43:11 +0100

    firefox and themeing. looks a but messy still.

--- flake.nix
+++ flake.nix
@@ -2,13 +2,12 @@
 
   description = "A decaratve nix config";
 
-
   nixConfig = {
-    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
     extra-trusted-public-keys = [
-      # cachix
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      # hyprland
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
   };
--- systems/modules/hyprland/default.nix
+++ systems/modules/hyprland/default.nix
@@ -1,18 +1,64 @@
-{ inputs, config, lib, pkgs, ... }:
+{ inputs, pkgs, ... }:
 {
+  services.xserver = {
+    displayManager.startx.enable = true;
+  };
 
   programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    xwayland.enable = true;
   };
 
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
+  };
+
+  security = {
+    polkit.enable = true;
+  };
+
+  environment.systemPackages = with pkgs.gnome; [
+    adwaita-icon-theme
+    nautilus
+    gnome-calendar
+    gnome-boxes
+    gnome-system-monitor
+    gnome-control-center
+    gnome-weather
+    gnome-calculator
+    gnome-software # for flatpak
+  ];
+
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+
+  services = {
+    gvfs.enable = true;
+    devmon.enable = true;
+    udisks2.enable = true;
+    upower.enable = true;
+    accounts-daemon.enable = true;
+    gnome = {
+      evolution-data-server.enable = true;
+      glib-networking.enable = true;
+      gnome-keyring.enable = true;
+      gnome-online-accounts.enable = true;
+    };
+  };
   environment.sessionVariables.NIXOS_OZONE_WL = "1"; # electron apps use wayland
-
-
-  nix.settings = {
-    substituters = [ "https://hyprland.cachix.org" ];
-    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
-  };
-
-
 }
