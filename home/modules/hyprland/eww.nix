884702f04c4f61e2af971549bbc93569d8b4849d
knoff <selby@niko.ink>
2024-01-27 18:26:15 +0100

    small work in progress making eww module

--- home/modules/hyprland/eww.nix
+++ home/modules/hyprland/eww.nix
@@ -1,8 +1,37 @@
 { config, pkgs, lib, ... }:
 {
-  programs.eww.enable = true;
-  programs.eww.configDir = ./eww;
 
-  programs.eww.package = pkgs.eww-wayland;
+  programs.eww = {
+    package = pkgs.eww-wayland;
+
+    enable = true;
+  #  configDir = ./eww;
+  };
+
+
+
+  #  home.file =
+  #    let
+  #      dir = "eww";
+  #    in
+  #    {
+  #      "${dir}/eww.yuck" = {
+  #        text = ''
+  #
+  #safasf asf
+  #
+  #      '';
+  #      };
+  #      "${dir}/eww.scss" = {
+  #        text = ''
+  #          @import "eww";
+  #
+  #          @include eww;
+  #        '';
+  #      };
+  #
+  #    };
+
+
 
 }
