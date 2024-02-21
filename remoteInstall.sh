a2d86ab8cc037e78d26cac9666dfbb0ad913ad3d
knoff <selby@niko.ink>
2024-02-21 23:16:40 +0100

    shell scripts fix

--- remoteInstall.sh
+++ remoteInstall.sh
@@ -1,17 +1,15 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash -p bash jq
-
-
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p bash jq
 
 set -euo pipefail
+
 
 function selectConfig() {
 
     configs=$(nix flake show --json | jq -r '.nixosConfigurations')
+
     # get the names of the configs
-    names=$(echo $configs | jq -r 'keys[]')
-
-
+    names=$(echo "$configs" | jq -r 'keys[]')
 
     # select the config
     i=0
@@ -24,7 +22,7 @@ function selectConfig() {
 
     # check if the input is valid
     if [[ ! $i =~ ^[0-9]+$ ]]; then
-        echo "Invalid input"
+        >&2 echo "Invalid input"
         exit 1
     fi
 
@@ -32,54 +30,59 @@ function selectConfig() {
     # bash array, num is the index
     i=0
     for name in $names; do
-        if [[ $i == $num ]]; then
+        if [[ $i == "$num" ]]; then
             config=$name
             break
         fi
         i=$((i+1))
     done
-
 }
 
 
-
-
 function selectComputer() {
-    # use nmap to scan the network
-    # get the ip addresses of the devices
-    # first get the ip structure of the network
-    # should output something like 192.168.XXX.0/24
-#    ip=$(ip route | grep default | awk '{print $3}')
-#    ip=${ip%.*}.0/24
-#
-#    nmap -sn $ip
-
-
-    # nix-shell -p nmap --run "nmap -sn 192.168.16.0/24"
-    # sho
-
-#    devices=$(nmap -sn )
-
     # ip address
     read -p "Enter the ip address: " ip
 
     # check if the ip address is valid
     if [[ ! $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-        echo "Invalid ip address"
+        >&2 echo "Invalid ip address"
         exit 1
     fi
 
-    # check if the ip address is reachable
-    if ! ping -c 1 $ip &> /dev/null; then
-        echo "Ip address not reachable"
-        exit 1
-    fi
-    echo $ip
+    echo "$ip"
 }
 
+
+# if argument is not -y or --yes, then ask the user if he wants to use the last options
+yn="y"
+
+# check if the argument is not empty
+if [[ -f .remoteInstallOptions ]] && [[ ! $# -gt 0 ]] || [[ $1 != "-y" && $1 != "--yes" ]]; then
+    # list contents of .remoteDeployOptions
+    jq . .remoteInstallOptions
+    read -p "Do you want to use the last options? [Y/n] " yn
+fi
+
+if [[ $yn == "Y" || $yn == "y" ]]; then
+    # restore the options from the last run
+    if [[ -f .remoteInstallOptions ]]; then
+        options=$(cat .remoteInstallOptions)
+        config=$(echo "$options" | jq -r '.config')
+        ip=$(echo "$options" | jq -r '.ip')
+    else
+        >&2 echo "No options to restore"
+        exit 1
+    fi
+else
+    # get the config
+    selectConfig
+    ip=$(selectComputer)
+    jq -n --arg config "$config" --arg ip "$ip" '{"config": $config, "ip": $ip}' > .remoteInstallOptions
+
+fi
 
 # get the config
 selectConfig
 ip=$(selectComputer)
 
-nix run github:nix-community/nixos-anywhere -- --flake .#$config root@$ip
+eval "nix run github:nix-community/nixos-anywhere -- --flake .#$config root@$ip"
