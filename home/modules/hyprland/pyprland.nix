f09a0e3beb1393b4060fba23694ff9f7c86c4e9e
knoff <selby@niko.ink>
2024-04-12 23:29:37 +0200

    pyprland module works
    i just submitted a pull request for it

--- home/modules/hyprland/pyprland.nix
+++ home/modules/hyprland/pyprland.nix
@@ -1,65 +1,42 @@
-{ pkgs, config, lib, ... }:
+{ options, config, lib, pkgs, ... }:
 let
   cfg = config.pyprland;
+  settingsFormat = pkgs.formats.toml {};
 in
 with lib;
 {
-  options = {
-    pyprland = {
-      enable = mkEnableOption "pyperland";
 
-      package = mkOption {
-        default = pkgs.pyprland;
-        type = types.package;
-        description = "The pyprland package to use";
-      };
+  options.pyprland = {
+    enable = mkEnableOption "pyperland";
 
-      extraPlugins = mkOption {
-        default = [];
-        type = with types; listOf str;
-        description = "Additional plugins to enable";
-      };
+    package = mkOption {
+      default = pkgs.pyprland;
+      type = types.package;
+      description = "The pyprland package to use";
+    };
 
-      config = mkOption {
-        default = {};
-        type = with types; attrsOf (oneOf [ str int bool ]);
-        description = "Additional pyprland configuration options";
-      };
+    extraPlugins = mkOption {
+      default = [];
+      type = with types; listOf str;
+      description = "Additional plugins to enable";
+    };
 
-      scratchpads = mkOption {
-        default = {};
-        type = with types; attrsOf (submodule {
-          options = {
-            animation = mkOption {
-              type = str;
-              description = "Animation type for the scratchpad";
-            };
-            command = mkOption {
-              type = str;
-              description = "Command to execute for the scratchpad";
-            };
-            class = mkOption {
-              type = str;
-              description = "Window class for the scratchpad";
-            };
-            lazy = mkOption {
-              type = bool;
-              default = false;
-              description = "Whether to lazily spawn the scratchpad";
-            };
-            size = mkOption {
-              type = str;
-              description = "Size of the scratchpad window";
-            };
-            unfocus = mkOption {
-              type = str;
-              default = "hide";
-              description = "Behavior when the scratchpad loses focus";
-            };
-          };
-        });
-        description = "Scratchpad configurations";
+    settings = mkOption {
+      type = types.submodule {
+        freeformType = settingsFormat.type;
+
+        options.scratchpads = mkOption {
+          default = {};
+          type = with types; attrsOf attrs;
+          description = "Scratchpad configurations";
+        };
       };
+      default = {};
+      description = ''
+        Configuration for pyprland, see
+        <link xlink:href="https://github.com/hyprland-community/pyprland/wiki/Getting-started#configuring"/>
+        for supported values.
+      '';
     };
   };
 
@@ -68,15 +45,15 @@ with lib;
       cfg.package
     ];
 
-    home.file."pyprland" = {
-      target = ".config/hypr/pyprland.toml";
-      source = pkgs.writers.writeTOML "pyprland.toml" (recursiveUpdate {
-        pyprland = {
-          plugins = cfg.extraPlugins ++ (optional (cfg.scratchpads != {}) "scratchpads");
-        };
-      } (recursiveUpdate cfg.config {
-        scratchpads = mapAttrs (name: value: removeAttrs value ["_module"]) cfg.scratchpads;
-      }));
+    pyprland.settings = {
+      pyprland = {
+        plugins = cfg.extraPlugins ++ (optional (cfg.settings.scratchpads != {}) "scratchpads");
+      };
     };
+
+    home.file.".config/hypr/pyprland.toml".source =
+      settingsFormat.generate "pyprland-config.toml" cfg.settings;
   };
 }
+
+
