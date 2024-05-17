c4b218fa701f871ddda5da06d5b72fb963745b4b
knoff <selby@niko.ink>
2024-05-17 11:56:12 +0200

    Refactor Firefox configuration for easier management
    - Remove specific profile configurations
    - Consolidate userChrome and userContent settings
    - Update search engine URLs and settings
    
    This commit simplifies the Firefox configuration by removing specific profile settings and consolidating userChrome and userContent configurations. It also updates search engine URLs and settings for better functionality.

--- home/programs/browser/firefox/default.nix
+++ home/programs/browser/firefox/default.nix
@@ -1,10 +1,57 @@
+{inputs, pkgs, theme, config, ...}:
 {
-  imports = [
-    ./profiles/main
-    ./profiles/minimal
-  ];
-
-  programs.firefox = {
+  programs.firefox =
+  {
     enable = true;
+    profiles."main" = {
+      isDefault = true;
+      id = 0;
+      name = "main";
+
+      # addons
+      extensions = import ./addons { inherit inputs pkgs; };
+
+      # custom search engines, default, etc.
+      search.engines = import ./searchEngines { inherit pkgs; };
+      search = {
+        force = true;
+        default = "duckduckgo";
+        order = [ "Annas-Archive" "NixOS Wiki" "Nix Packages" "Nix Options" "Home-Manager" "StackOverflow" "Github" "fmhy" ];
+      };
+
+      # theme for the firefox ui
+      userChrome = import ./userChrome { inherit theme pkgs; };
+
+      # theme for the content firefox presents.
+      userContent = import ./userContent { inherit theme; };
+
+      # settings for firefox. telemetry, scrolling, etc.
+      settings = import ./settings { inherit theme; };
+    };
+    profiles."minimal" = {
+      isDefault = false;
+      id = 1;
+      name = "minimal";
+
+      # addons
+      extensions = import ./addons/minimal.nix { inherit inputs pkgs; };
+
+      # custom search engines, default, etc.
+      search.engines = import ./searchEngines { inherit pkgs; };
+      search = {
+        force = true;
+        default = "duckduckgo";
+        order = [ "Annas-Archive" "NixOS Wiki" "Nix Packages" "Nix Options" "Home-Manager" "StackOverflow" "Github" "fmhy" ];
+      };
+
+      # theme for the firefox ui
+      userChrome = import ./userChrome { inherit theme pkgs; };
+
+      # theme for the content firefox presents.
+      userContent = import ./userContent { inherit theme; };
+
+      # settings for firefox. telemetry, scrolling, etc.
+      settings = import ./settings { inherit theme; };
+    };
   };
 }
