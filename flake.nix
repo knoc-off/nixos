41ced74cee3371cffb2a3fe7080ef2f601121584
knoff <selby@niko.ink>
2024-07-01 12:21:17 +0200

    commmit. portfolio still not working

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Rust overlay
     rust-overlay.url = "github:oxalica/rust-overlay";
@@ -17,7 +20,7 @@
 
     # Home Manager
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
--- pkgs/portfolio/src/components/resume.rs
+++ pkgs/portfolio/src/components/resume.rs
@@ -6,11 +6,11 @@ impl Component for Resume {
     type Message = ();
     type Properties = ();
 
-    fn create(ctx: &Context<Self>) -> Self {
+    fn create(_ctx: &Context<Self>) -> Self {
         Resume
     }
 
-    fn view(&self, ctx: &Context<Self>) -> Html {
+    fn view(&self, _ctx: &Context<Self>) -> Html {
         html! {
             <div>
                 <h1>{ "New Page" }</h1>
