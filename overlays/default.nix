af05553daa82bc92f90e6d381ad7ce65c615b2be
knoff <selby@niko.ink>
2024-07-06 12:39:38 +0200

    Refactor project languages display and add new projects to resume
    - Refactored the display of project languages in the resume component to include a hover effect showing the language and percentage.
    - Added new projects to the resume data including Nix Configurations and Yew Website.
    - Each project includes details such as icon, name, description, languages used, and a link to the project repository.
    - Updated project information in the resume page with the newly added projects and their details.

--- overlays/default.nix
+++ overlays/default.nix
@@ -23,13 +23,13 @@
 
   modifications = _final: prev: {
 
-    #spotiblock = prev.spotify.overrideAttrs (_old: rec {
-    #  postInstall = ''
-    #    ExecMe="env LD_PRELOAD=${prev.spotify-adblock}/lib/libspotifyadblock.so spotify"
-    #    sed -i "s|^TryExec=.*|TryExec=$ExecMe %U|" $out/share/applications/spotify.desktop
-    #    sed -i "s|^Exec=.*|Exec=$ExecMe %U|" $out/share/applications/spotify.desktop
-    #  '';
-    #});
+    spotiblock = prev.spotify.overrideAttrs (_old: rec {
+      postInstall = ''
+        ExecMe="env LD_PRELOAD=${prev.spotify-adblock}/lib/libspotifyadblock.so spotify"
+        sed -i "s|^TryExec=.*|TryExec=$ExecMe %U|" $out/share/applications/spotify.desktop
+        sed -i "s|^Exec=.*|Exec=$ExecMe %U|" $out/share/applications/spotify.desktop
+      '';
+    });
 
     steam-scaling = prev.steamPackages.steam-fhsenv.override (old: rec {
       extraArgs = (old.extraArgs or "") + " -forcedesktopscaling 1.0 ";
