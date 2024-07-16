eda4fbb11c24dea630c371439219faa024f6817e
knoff <selby@niko.ink>
2024-07-16 16:36:39 +0200

    Refactor package definitions and overlays in flake.nix and pkgs/default.nix
    - Move package definitions and overlays from the flake.nix to pkgs/default.nix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,36 +1,47 @@
 { pkgs, inputs, ... }: {
 
-  spotify-adblock = pkgs.callPackage ./spotify-adblock {};
-  llm-cmd = pkgs.python3Packages.callPackage ./llm-cmd {};
-  ttok = pkgs.python3Packages.callPackage ./ttok {};
-  poe = pkgs.python3Packages.callPackage ./poe-llm-api {};
-  gate = pkgs.callPackage ./gate {};
-  ascii-silhouettify = pkgs.callPackage ./ascii {};
+  spotify-adblock = pkgs.callPackage ./spotify-adblock { };
+  llm-cmd = pkgs.python3Packages.callPackage ./llm-cmd { };
+  ttok = pkgs.python3Packages.callPackage ./ttok { };
+  poe = pkgs.python3Packages.callPackage ./poe-llm-api { };
+  gate = pkgs.callPackage ./gate { };
+  ascii-silhouettify = pkgs.callPackage ./ascii { };
 
+  neovim-nix = let
+    nixvim = inputs.nixvim.legacyPackages.${pkgs.system};
 
-   website =
-   let
-     #rust-overlay = inputs.rust-overlay;
-     rustPkgs = pkgs.extend (import inputs.rust-overlay);
-   in
-   {
-     portfolio = rustPkgs.callPackage ./portfolio { };
-   };
+    customPkgs = import inputs.nixpkgs-unstable {
+      inherit (pkgs) system;
+      config = { allowUnfree = true; };
+      overlays = [ inputs.nixneovimplugins.overlays.default ];
+    };
 
-   # This lets me use font glyphs as SVG's for places that dont accept SVG.
-   material-icons-ext = import ./svg-tools/icon-extractor {inherit pkgs;
-    fontPath = "${pkgs.material-icons}/share/fonts/opentype/MaterialIconsRound-Regular.otf";
-   };
+  in {
+    default = nixvim.makeNixvimWithModule {
+      pkgs = customPkgs;
+      module = import ./neovim/configurations;
+    };
+  };
 
-   writeNuScript =
-     name:
-       (script:
-         pkgs.writeTextFile rec {
-           inherit name;
-           text = "#!${pkgs.nushell}/bin/nu" + "\n" + script;
+  website = let
+    #rust-overlay = inputs.rust-overlay;
+    rustPkgs = pkgs.extend (import inputs.rust-overlay);
+  in { portfolio = rustPkgs.callPackage ./portfolio { }; };
 
-           executable = true;
-           destination = "/bin/${name}";
-         }
-       );
+  # This lets me use font glyphs as SVG's for places that dont accept SVG.
+  material-icons-ext = import ./svg-tools/icon-extractor {
+    inherit pkgs;
+    fontPath =
+      "${pkgs.material-icons}/share/fonts/opentype/MaterialIconsRound-Regular.otf";
+  };
+
+  writeNuScript = name:
+    (script:
+      pkgs.writeTextFile rec {
+        inherit name;
+        text = "#!${pkgs.nushell}/bin/nu" + "\n" + script;
+
+        executable = true;
+        destination = "/bin/${name}";
+      });
 }
