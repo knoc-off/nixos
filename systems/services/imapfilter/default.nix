0357a87f98219f40b7a310683ee0b82c60fd3860
knoff <selby@niko.ink>
2024-08-08 06:54:18 +0200

    modified my imapfilter service
    it now takes a directory as input, where you can define all of your
    scripts/filters. each one is self-contained, and wrapped in a scope.
    
    currently there is just a test example

--- systems/services/imapfilter/default.nix
+++ systems/services/imapfilter/default.nix
@@ -1,19 +1,15 @@
 { inputs, config, pkgs, ... }:
 
 let
-  # Get all .lua files from the filters directory
-  filterFiles = builtins.filter (n: builtins.match ".*\\.lua" n != null)
-    (builtins.attrNames (builtins.readDir ./filters));
+  filterConfigs = import ./filter-concat.nix { inherit pkgs; };
 
-  # Create a list of paths to all .lua files
-  filterPaths = map (file: ./filters + "/${file}") filterFiles;
-
-  # Script to combine all config files
   combineConfigs = pkgs.writeShellScript "combine-configs.sh" ''
-    temp=$(mktemp)
-    cat ${config.sops.secrets."services/imap/imapfilter_config".path} ${builtins.toString filterPaths} > "$temp"
+    #temp=$(mktemp)
+    temp="/tmp/imapfilter_config"
+    cat ${config.sops.secrets."services/imap/imapfilter_config".path} ${filterConfigs}/concatenated_filters.lua > "$temp"
     echo "$temp"
   '';
+
 in {
   sops.secrets = { "services/imap/imapfilter_config" = { }; };
   # EXAMPLE YAML:
@@ -26,22 +22,17 @@ in {
   #                password = 'pa$$w0rd',
   #              }
 
-  # Ensure required packages are installed
-  environment.systemPackages = with pkgs; [ imapfilter sops ];
-
   systemd.services.imapfilter = {
     description = "IMAPFilter mail filtering service";
     after = [ "network-online.target" ];
     wants = [ "network-online.target" ];
     serviceConfig = {
       Type = "oneshot";
-      ExecStart =
-        "${pkgs.bash}/bin/bash -c '${pkgs.imapfilter}/bin/imapfilter -c $(${combineConfigs})'";
+      ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.imapfilter}/bin/imapfilter -c $(${combineConfigs})'";
       User = "root";
     };
   };
 
-  # Might want to disable this
   systemd.timers.imapfilter = {
     wantedBy = [ "timers.target" ];
     timerConfig = {
