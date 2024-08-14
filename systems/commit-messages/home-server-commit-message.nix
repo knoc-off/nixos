2b6fffcbb5dedfd747e7a8662a0bfbd0594b7b93
knoff <selby@niko.ink>
2024-08-14 23:02:28 +0200

    test commit for

--- systems/commit-messages/home-server-commit-message.nix
+++ systems/commit-messages/home-server-commit-message.nix
@@ -1,3 +1,3 @@
 {
-  system.nixos.label = "added_the_commit_message_to_home-server___________";
+  system.nixos.label = "added_the_commit_message_to_home-server______________________________________________test_commit_for";
 }
