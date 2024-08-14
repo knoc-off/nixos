522da05f35fd7f270a24db25e0c6e298442d8d02
knoff <selby@niko.ink>
2024-08-14 23:33:06 +0200

    re-added pyprland for the time being

--- systems/commit-messages/desktop-commit-message.nix
+++ systems/commit-messages/desktop-commit-message.nix
@@ -1,3 +1,3 @@
 {
-  system.nixos.label = "fixed_the_way_the_commit-message_is_formatted________________________________________test_commit_for";
+  system.nixos.label = "fixed_the_way_the_commit-message_is_formatted___________________re-added_pyprland_for_the_time_being";
 }
--- systems/commit-messages/home-server-commit-message.nix
+++ systems/commit-messages/home-server-commit-message.nix
@@ -1,3 +1,3 @@
 {
-  system.nixos.label = "added_the_commit_message_to_home-server______________________________________________test_commit_for";
+  system.nixos.label = "added_the_commit_message_to_home-server_________________________re-added_pyprland_for_the_time_being";
 }
