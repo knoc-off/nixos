4a146b2c61e7dc8d0cca31d8daf8ed2d5378f100
knoff <selby@niko.ink>
2024-08-14 23:02:28 +0200

    test commit for

--- systems/commit-messages/hetzner-server-commit-message.nix
+++ systems/commit-messages/hetzner-server-commit-message.nix
@@ -1,3 +1,3 @@
 {
-  system.nixos.label = "fixed_zsh_issue_added_imapfilter_stuff____________";
+  system.nixos.label = "fixed_zsh_issue_added_imapfilter_stuff_______________________________________________test_commit_for";
 }
--- systems/commit-messages/raspberry3B-commit-message.nix
+++ systems/commit-messages/raspberry3B-commit-message.nix
@@ -1,3 +1,3 @@
 {
-  system.nixos.label = "fixed_the_way_the_commit-message_is_formatted_____";
+  system.nixos.label = "fixed_the_way_the_commit-message_is_formatted________________________________________test_commit_for";
 }
