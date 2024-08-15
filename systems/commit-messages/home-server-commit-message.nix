11d6303425c511dbf72ed77179b623dd8ec9b4ce
knoff <selby@niko.ink>
2024-08-15 13:17:33 +0200

    fully moved away from pyprland

--- systems/commit-messages/home-server-commit-message.nix
+++ systems/commit-messages/home-server-commit-message.nix
@@ -1,3 +1,3 @@
 {
-  system.nixos.label = "added_the_commit_message_to_home-server_____________________________________shift_away_from_pyprland";
+  system.nixos.label = "added_the_commit_message_to_home-server_______________________________fully_moved_away_from_pyprland";
 }
