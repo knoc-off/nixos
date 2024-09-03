3bcc7a32c2c22ee843fdd8908a7eb4cafb0d8f1a
knoff <selby@niko.ink>
2024-09-03 23:48:25 +0200

    fix: the post-commit, message being the body

--- systems/commit-messages/desktop-commit-message.nix
+++ systems/commit-messages/desktop-commit-message.nix
@@ -1,3 +1,3 @@
 {
-  system.nixos.label = "fix:_spotify-adblock_working_________________________________________________________________________________________________________________________________________";
+  system.nixos.label = "fixed_the_way_the_commit-message_is_formatted___________fix:_the_post-commit_message_being_the_body_";
 }
