725d6d9359bcbb8260f4733ca3672b07641130d4
knoff <selby@niko.ink>
2024-09-10 17:40:21 +0200

    git ignore added.

--- systems/commit-messages/framework13-commit-message.nix
+++ systems/commit-messages/framework13-commit-message.nix
@@ -1,3 +1,3 @@
 {
-  system.nixos.label = "added_dialout_group._added_c_cross________________Add_ATtiny85_development_environment_for_Digispark";
+  system.nixos.label = "added_dialout_group._added_c_cross_________________________________________________git_ignore_added.";
 }
