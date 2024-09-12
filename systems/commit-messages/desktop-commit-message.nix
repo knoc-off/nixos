ad86b9910cbd75fb258ae3be0066398781e06089
knoff <selby@niko.ink>
2024-09-12 12:35:16 +0200

    gitignore

--- systems/commit-messages/desktop-commit-message.nix
+++ systems/commit-messages/desktop-commit-message.nix
@@ -1,3 +1,3 @@
 {
-  system.nixos.label = "fixed_the_way_the_commit-message_is_formatted____________________some_wip_packages_and_gtk_settings_";
+  system.nixos.label = "fixed_the_way_the_commit-message_is_formatted______________________________________________gitignore";
 }
