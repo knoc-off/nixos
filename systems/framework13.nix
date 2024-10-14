d85d4eadace16e61473b68ca9621825e31c1782e
knoff <selby@niko.ink>
2024-10-14 13:39:30 +0200

    removed stylix

--- systems/framework13.nix
+++ systems/framework13.nix
@@ -23,20 +23,6 @@
     #  };
     #}
 
-    # Stylix ~~ \/
-
-    #inputs.stylix.nixosModules.stylix
-    #{
-    #  stylix.image = pkgs.fetchurl {
-    #    url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
-    #    sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
-    #  };
-    #  stylix.polarity = "dark";
-    #}
-
-    # Stylix ~~ /\
-
-
     inputs.hardware.nixosModules.framework-13-7040-amd
 
     inputs.disko.nixosModules.disko
