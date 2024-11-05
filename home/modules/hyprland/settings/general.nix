aefb44c6c08836b1fe9cbf4605b4b641485d3c23
knoff <selby@niko.ink>
2024-11-05 13:53:50 +0100

    user is passed to the main function. and small system changes

--- home/modules/hyprland/settings/general.nix
+++ home/modules/hyprland/settings/general.nix
@@ -1,5 +1,6 @@
 {
   config,
+  user,
   theme,
   colorLib,
   pkgs,
--- systems/modules/shell/bash.nix
+++ systems/modules/shell/bash.nix
@@ -1,6 +1,9 @@
 { config, pkgs, lib, ... }:
 {
   programs.bash = {
+    shellAliases = {
+      wgnord = "sudo ${pkgs.wgnord}/bin/wgnord";
+    };
     interactiveShellInit = ''
       if [ -f ~/.profile ]; then
         . ~/.profile
