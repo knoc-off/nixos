9c8567395d4fd2acab4028cac60fa15917aca0c4
knoff <selby@niko.ink>
2024-11-23 16:38:51 +0100

    changes made to neovim and lsp.
    neovim errors when characters are not in utf-8.

--- pkgs/neovim/configurations/plugins/ls/languages/nix.nix
+++ pkgs/neovim/configurations/plugins/ls/languages/nix.nix
@@ -1,16 +1,32 @@
-{
+{ pkgs, ... }: {
   plugins = {
-    nix.enable = true;
-    nix-develop.enable = true;
-    lsp.servers.nixd.enable = true;
-    none-ls = {
-      sources = {
-        formatting.nixfmt.enable = true;
-        diagnostics = {
-          statix.enable = true;
-          deadnix.enable = true;
+    #nix.enable = true;
+    #nix-develop.enable = true;
+    #lsp.servers.nixd.enable = true;
+    lsp.servers.nixd = {
+      enable = true;
+      settings = {
+        formatting.command = [ "nixfmt" ];
+        nixpkgs.expr = "import <nixpkgs> { }";
+
+        options = let flake = ''(builtins.getFlake "/etc/nixos)"'';
+        in {
+          # Completitions for nixos and home manager options
+          nixos.expr = "${flake}.nixosConfigurations.framework13.options";
+          home_manager.expr = "${flake}.homeConfigurations.framework13.options";
+
+          nixvim.expr = "${flake}.packages.${pkgs.system}.neovim-nix.default.options";
         };
       };
     };
+    #none-ls = {
+    #  sources = {
+    #    formatting.nixfmt.enable = true;
+    #    diagnostics = {
+    #      statix.enable = true;
+    #      deadnix.enable = true;
+    #    };
+    #  };
+    #};
   };
 }
