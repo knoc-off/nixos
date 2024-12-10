a8670ee619cb0f075446973e264a9396202f652f
knoff <selby@niko.ink>
2024-12-10 10:17:44 +0100

    imap filter, removed

--- systems/services/imapfilter/filters/mail-tagger.lua
+++ systems/services/imapfilter/filters/mail-tagger.lua
@@ -1,31 +1,52 @@
 -- Priority: 3
 -- Function to tag emails based on the sender's email address.
 
--- table:
--- EmailTags = {
---     ["a@example.com"] = { "tag1", "tag2" },
---     ["b@example.com"] = { "tag2", "tag3" },
---     ["c@example.com"] = { "tag1", "tag3" },
---     ["d@example.com"] = { "tag1", "tag2", "tag3" },
---     ["e@example.com"] = { "tag2" },
---     ["f@example.com"] = { "tag1" },
---     ["g@example.com"] = { "tag3" },
--- }
--- Assume EmailTags is defined in the global scope.
---
-
 function filter()
     messages = account.INBOX:select_all()
-    for email, tags in pairs(EmailTags) do
+    for email, tags in pairs(emailsFrom) do
         local from_matches = messages:contain_from(email)
-        local to_matches = messages:contain_to(email)
-
         if from_matches then
             from_matches:add_flags(tags)
         end
+    end
+    for email, tags in pairs(emailsTo) do
+        local to_matches = messages:contain_to(email)
         if to_matches then
             to_matches:add_flags(tags)
         end
     end
+
+
+    for _, mesg in ipairs(messages) do
+        mbox, uid = table.unpack(mesg)
+        flags = mbox[uid]:fetch_flags()
+
+        -- recursively print the Flags
+        function print_flags(flags)
+            for k, v in pairs(flags) do
+                if type(v) == "table" then
+                    print(k)
+                    print_flags(v)
+                else
+                    print(k, v)
+                end
+            end
+        end
+
+        print_flags(flags)
+
+        print("\n-----------------------------------------------\n")
+    end
+
+
+
+
 end
+
+-- become_daemon(60 * 2, filter) -- every 2 minutes
+filter()
+
+
+
+
 
