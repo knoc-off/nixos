616378913214e6747150b497e28c337c7760008c
knoff <selby@niko.ink>
2024-12-21 15:27:30 +0100

    bevy example

--- systems/commit-messages/desktop-commit-message.nix
+++ systems/commit-messages/desktop-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "minecraft_changes._disable_waydroid_________________________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_____________________________________________________moving_to_new_version_of_nixpkgs._24.11";
+  system.nixos.label = "bevy_example________________________________________________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_____________________________________________________moving_to_new_version_of_nixpkgs._24.11";
 }
