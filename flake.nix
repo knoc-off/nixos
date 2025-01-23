6048c1cd2f35d1e4745f176945708eebd48e159e
knoff <selby@niko.ink>
2025-01-23 12:59:19 +0100

    System update

--- flake.nix
+++ flake.nix
@@ -158,7 +158,7 @@
 
     # Hyprland
     #hyprland.url = "github:hyprwm/hyprland";
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    hyprland.url = "github:hyprwm/Hyprland";
     hyprland-plugins = {
       url = "github:hyprwm/hyprland-plugins";
       inputs.hyprland.follows = "hyprland";
--- pkgs/website/axum/default.nix
+++ pkgs/website/axum/default.nix
@@ -1,10 +1,19 @@
 { pkgs, rustPlatform, lib, ... }:
 let
+
+
+  htmxJs = pkgs.fetchurl {
+    url = "https://unpkg.com/htmx.org@1.9.2/dist/htmx.min.js";
+    hash = "sha256-/TRunIY51GJIk/xFXyQHoJtBgwFzbdGOu7B3ZGN/tHg=";
+  };
+
+
   fonts = [
     {
       name = "MaterialIconsRound";
       package = pkgs.material-icons;
-      file = "${pkgs.material-icons}/share/fonts/opentype/MaterialIconsRound-Regular.otf";
+      file =
+        "${pkgs.material-icons}/share/fonts/opentype/MaterialIconsRound-Regular.otf";
       format = "opentype";
       base_class = "mi-round";
       prefix = "MIRound_";
@@ -12,17 +21,15 @@ let
     {
       name = "MaterialSymbolsRounded";
       package = pkgs.material-symbols;
-      file = "${pkgs.material-symbols}/share/fonts/TTF/MaterialSymbolsRounded.ttf";
+      file =
+        "${pkgs.material-symbols}/share/fonts/TTF/MaterialSymbolsRounded.ttf";
       format = "opentype";
       base_class = "ms-round";
       prefix = "MS_";
     }
   ];
 
-  templatePatterns = [
-    "./templates/*.html"
-    "./templates/**/*.html"
-  ];
+  templatePatterns = [ "./templates/*.html" "./templates/**/*.html" ];
 
   iconProcessor = import ./icon-processor.nix {
     inherit pkgs fonts templatePatterns;
@@ -46,7 +53,6 @@ let
       --minify
   '';
 
-
   switchToProdScript = pkgs.writeScriptBin "switch-to-prod" ''
     echo "Switching to production (pruned) assets..."
 
@@ -58,11 +64,18 @@ let
     ln -sf ${pkgs.circle-flags}/share/circle-flags-svg/ ./static/icons/circle-flags-svg
     ln -sf ${pkgs.super-tiny-icons}/share/icons/SuperTinyIcons/svg/ ./static/icons/super-tiny-icons
 
-    # Copy production-optimized assets
+    # Copy production-optimized assets with write permissions
     cp -r ${iconProcessor}/share/fonts/* ./static/fonts/
+    chmod -R u+w ./static/fonts
+
     cp -r ${iconProcessor}/share/data/* ./data/
+    chmod -R u+w ./data
+
     cp -r ${iconProcessor}/share/css/* ./static/css/
+    chmod -R u+w ./static/css
+
     cp -r ${iconProcessor}/debug/* ./debug/
+    chmod -R u+w ./debug
 
     # Rebuild CSS
     ${cssBuildScript}/bin/tailwind-build
@@ -81,11 +94,18 @@ let
     ln -sf ${pkgs.circle-flags}/share/circle-flags-svg/ ./static/icons/circle-flags-svg
     ln -sf ${pkgs.super-tiny-icons}/share/icons/SuperTinyIcons/svg/ ./static/icons/super-tiny-icons
 
-    # Copy development assets
+    # Copy development assets with write permissions
     cp -r ${iconProcessorDev}/share/fonts/* ./static/fonts/
+    chmod -R u+w ./static/fonts
+
     cp -r ${iconProcessorDev}/share/data/* ./data/
+    chmod -R u+w ./data
+
     cp -r ${iconProcessorDev}/share/css/* ./static/css/
+    chmod -R u+w ./static/css
+
     cp -r ${iconProcessorDev}/debug/* ./debug/
+    chmod -R u+w ./debug
 
     # Rebuild CSS
     ${cssBuildScript}/bin/tailwind-build
@@ -93,11 +113,10 @@ let
     echo "Switched to development assets. Debug information available in ./debug/"
   '';
 
-
   devSetupScript = pkgs.writeScriptBin "setup-dev-env" ''
     # Clean and create directories
     rm -rf ./static/fonts ./static/icons ./data
-    mkdir -p ./static/fonts ./static/icons ./static/css ./data
+    mkdir -p ./static/fonts ./static/icons ./static/css ./static/js ./data
 
     # Link static content
     ln -sf ${pkgs.circle-flags}/share/circle-flags-svg/ ./static/icons/circle-flags-svg
@@ -107,6 +126,9 @@ let
     cp -r ${iconProcessorDev}/share/fonts/* ./static/fonts/
     cp -r ${iconProcessorDev}/share/data/* ./data/
     cp -r ${iconProcessorDev}/share/css/* ./static/css/
+
+    # Copy htmx
+    cp ${htmxJs} ./static/js/htmx.min.js
 
     # Build CSS
     ${cssBuildScript}/bin/tailwind-build
@@ -131,15 +153,15 @@ in rustPlatform.buildRustPackage rec {
     switchToDevScript
   ];
 
-  buildInputs = with pkgs; [
-    openssl.dev
-    pkg-config
-    zlib.dev
-  ];
+  buildInputs = with pkgs; [ openssl.dev pkg-config zlib.dev ];
 
   preBuild = ''
     # Create required directories
-    mkdir -p ./static/fonts ./static/css ./data
+    mkdir -p ./static/fonts ./static/css ./data ./static/js
+
+
+    # Copy htmx
+    cp ${htmxJs} ./static/js/htmx.min.js
 
     # Copy production-optimized fonts and data
     cp -r ${iconProcessor}/share/fonts/* ./static/fonts/
@@ -155,7 +177,8 @@ in rustPlatform.buildRustPackage rec {
   '';
 
   meta = with lib; {
-    description = "An Axum web application with Tailwind CSS integrated via Nix build";
+    description =
+      "An Axum web application with Tailwind CSS integrated via Nix build";
     homepage = "https://example.com";
     license = licenses.mit;
     maintainers = with maintainers; [ knoff ];
