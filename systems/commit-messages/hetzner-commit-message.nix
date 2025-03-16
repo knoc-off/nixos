0a24b3c93cc789faebf9e4d85df7732d3d95d331
knoff <selby@niko.ink>
2025-03-16 17:37:21 +0100

    wireguard

--- systems/commit-messages/hetzner-commit-message.nix
+++ systems/commit-messages/hetzner-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "fish________________________________________________________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "______________________________________added_blogging_ability_to_the_website._simple_markdown";
+  system.nixos.label = "wireguard___________________________________________________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "___________________________________________________________________________________wireguard";
 }
--- systems/commit-messages/raspberry-3b-commit-message.nix
+++ systems/commit-messages/raspberry-3b-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "fish________________________________________________________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________minecraft_changes._disable_waydroid";
+  system.nixos.label = "wireguard___________________________________________________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________minecraft_changes._disable_waydroid";
 }
