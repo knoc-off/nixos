62a79a68705f635411c33ac01ac809039e1f7fd0
knoff <selby@niko.ink>
2025-03-20 08:49:07 +0100

    feat: add lsp configuration files

--- systems/commit-messages/desktop-commit-message.nix
+++ systems/commit-messages/desktop-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "feat:_Add_keybind_to_accept_LSP_code_actions_under_cursorline_______________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_____________________________________________________moving_to_new_version_of_nixpkgs._24.11";
+  system.nixos.label = "feat:_add_lsp_configuration_files___________________________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_____________________________________________________moving_to_new_version_of_nixpkgs._24.11";
 }
--- systems/commit-messages/raspberry-3b-commit-message.nix
+++ systems/commit-messages/raspberry-3b-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "feat:_Add_keybind_to_accept_LSP_code_actions_under_cursorline_______________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________minecraft_changes._disable_waydroid";
+  system.nixos.label = "feat:_add_lsp_configuration_files___________________________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________minecraft_changes._disable_waydroid";
 }
