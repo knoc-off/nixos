1e005c81eb10118fcfa0066a5a986f456f74f902
knoff <selby@niko.ink>
2025-04-01 16:56:26 +0200

    refactor: Refactor adr script for improved argument parsing and readability

--- systems/commit-messages/framework13-commit-message.nix
+++ systems/commit-messages/framework13-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "feat:_Update_flake.lock_and_add_fish_alias_for_aider________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "________________________________________feat:_Update_flake.lock_and_add_fish_alias_for_aider";
+  system.nixos.label = "refactor:_Refactor_adr_script_for_improved_argument_parsing_and_readability_________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "________________________________________feat:_Update_flake.lock_and_add_fish_alias_for_aider";
 }
