75cfb3f6dbf183fc14bd4c3d29e11ef88f9b5209
knoff <selby@niko.ink>
2025-04-08 16:13:12 +0200

    fix: Use single-line strings for Lua in keymappings.nix

--- systems/commit-messages/framework13-commit-message.nix
+++ systems/commit-messages/framework13-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "fix:_Correct_Lua_syntax_in_Nix_string_for_keymappings_______________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
+  system.nixos.label = "fix:_Use_single-line_strings_for_Lua_in_keymappings.nix_____________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
 }
