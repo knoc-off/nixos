60fc7538ca3eade219341f2797e87c11410b9769
knoff <selby@niko.ink>
2025-04-08 17:38:40 +0200

    fix: Update fmod tests to match implementation's dividend sign behavior

--- systems/commit-messages/framework13-commit-message.nix
+++ systems/commit-messages/framework13-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "Refactor:_Group_math_tests_into_attribute_set_for_better_structure__________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
+  system.nixos.label = "fix:_Update_fmod_tests_to_match_implementations_dividend_sign_behavior______________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
 }
