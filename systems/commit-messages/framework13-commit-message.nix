8a36431a01a64866af0b4dc63c8ededc9f214860
knoff <selby@niko.ink>
2025-04-08 17:36:17 +0200

    fix: Correctly parse floating-point numbers using fromTOML

--- systems/commit-messages/framework13-commit-message.nix
+++ systems/commit-messages/framework13-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "fix:_Correctly_parse_floats_in_parseFloat_function__________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
+  system.nixos.label = "fix:_Correctly_parse_floating-point_numbers_using_fromTOML__________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
 }
--- systems/commit-messages/raspberry-3b-commit-message.nix
+++ systems/commit-messages/raspberry-3b-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "fix:_Correctly_parse_floats_in_parseFloat_function__________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________minecraft_changes._disable_waydroid";
+  system.nixos.label = "fix:_Correctly_parse_floating-point_numbers_using_fromTOML__________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________minecraft_changes._disable_waydroid";
 }
