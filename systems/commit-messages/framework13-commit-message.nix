9cf8d2a8165e6de38c686ee3b1454b53aa67d541
knoff <selby@niko.ink>
2025-04-08 16:12:19 +0200

    fix: Correct Lua syntax in Nix string for keymappings

--- systems/commit-messages/framework13-commit-message.nix
+++ systems/commit-messages/framework13-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "feat:_Implement_no-jump_search_with__and__keys_in_normal_mode_______________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
+  system.nixos.label = "fix:_Correct_Lua_syntax_in_Nix_string_for_keymappings_______________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
 }
