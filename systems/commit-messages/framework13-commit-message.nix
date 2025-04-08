89dbf6611b2b9e7a9e6897ce7bfe67df95d580a3
knoff <selby@niko.ink>
2025-04-08 17:12:18 +0200

    feat: Add math-tests.nix library

--- systems/commit-messages/framework13-commit-message.nix
+++ systems/commit-messages/framework13-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "feat:_Add_value_and_e2e_hex_conversions_to_color_tests._____________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
+  system.nixos.label = "feat:_Add_math-tests.nix_library____________________________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
 }
