dafea0ae74ea622b935dde2e853229d9e602d9e0
knoff <selby@niko.ink>
2025-04-08 16:29:26 +0200

    chore: Disable mypy in none-ls config, rely on pyright for types

--- systems/commit-messages/framework13-commit-message.nix
+++ systems/commit-messages/framework13-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "feat:_Set_pyright_missing_type_stubs_diagnostic_to_warning__________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
+  system.nixos.label = "chore:_Disable_mypy_in_none-ls_config_rely_on_pyright_for_types_____________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
 }
