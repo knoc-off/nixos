6dc6031134840c76759db539155d224a84c4f837
knoff <selby@niko.ink>
2025-04-08 12:55:20 +0200

    refactor: Remove which-key plugin settings from keymappings.nix

--- systems/commit-messages/framework13-commit-message.nix
+++ systems/commit-messages/framework13-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "feat:_Update_aider_model_and_starship_prompt_add_postgresql_________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
+  system.nixos.label = "refactor:_Remove_which-key_plugin_settings_from_keymappings.nix_____________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
 }
