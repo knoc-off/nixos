c968817fab119086854e5930b99910b4e90c7a61
knoff <selby@niko.ink>
2025-04-08 16:13:12 +0200

    fix: Use single-line strings for Lua in keymappings.nix

--- systems/commit-messages/desktop-commit-message.nix
+++ systems/commit-messages/desktop-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "fix:_Correct_Lua_syntax_in_Nix_string_for_keymappings_______________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_____________________________________________________moving_to_new_version_of_nixpkgs._24.11";
+  system.nixos.label = "fix:_Use_single-line_strings_for_Lua_in_keymappings.nix_____________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_____________________________________________________moving_to_new_version_of_nixpkgs._24.11";
 }
