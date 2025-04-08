e91cedb1a6c5848c39f6f916b2c071d405232214
knoff <selby@niko.ink>
2025-04-08 17:05:21 +0200

    feat: Add value and e2e hex conversions to color tests.

--- systems/commit-messages/desktop-commit-message.nix
+++ systems/commit-messages/desktop-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "chore:_Disable_mypy_in_none-ls_config_rely_on_pyright_for_types_____________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_____________________________________________________moving_to_new_version_of_nixpkgs._24.11";
+  system.nixos.label = "feat:_Add_value_and_e2e_hex_conversions_to_color_tests._____________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_____________________________________________________moving_to_new_version_of_nixpkgs._24.11";
 }
