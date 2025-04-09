3f4bddceb294d3841487f53f580f29c1c8796307
knoff <selby@niko.ink>
2025-04-09 18:52:05 +0200

    fix: Correct toe and toe_inv test expectations in color-tests.nix

--- systems/commit-messages/framework13-commit-message.nix
+++ systems/commit-messages/framework13-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "fix:_Relax_tolerance_for_boundary_check_in_color_tests______________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
+  system.nixos.label = "fix:_Correct_toe_and_toe_inv_test_expectations_in_color-tests.nix___________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
 }
--- systems/commit-messages/raspberry-3b-commit-message.nix
+++ systems/commit-messages/raspberry-3b-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "fix:_Relax_tolerance_for_boundary_check_in_color_tests______________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________minecraft_changes._disable_waydroid";
+  system.nixos.label = "fix:_Correct_toe_and_toe_inv_test_expectations_in_color-tests.nix___________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________minecraft_changes._disable_waydroid";
 }
