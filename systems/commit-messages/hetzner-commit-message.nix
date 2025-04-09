23f0e24c8d6ff1421a604043ce63e0f86870f573
knoff <selby@niko.ink>
2025-04-09 21:18:37 +0200

    refactor: Rename colorLib to color-lib for consistency

--- systems/commit-messages/hetzner-commit-message.nix
+++ systems/commit-messages/hetzner-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "refactor:_Rename_lighten_function_to_setLightness_for_clarity_______________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "___________________________________________________________________________________wireguard";
+  system.nixos.label = "refactor:_Rename_colorLib_to_color-lib_for_consistency______________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "___________________________________________________________________________________wireguard";
 }
