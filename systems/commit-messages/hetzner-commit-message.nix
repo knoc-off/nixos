a59438620f70f53688b6eda36e438c8003c7d212
knoff <selby@niko.ink>
2025-04-09 19:33:39 +0200

    refactor: Remove sRGB transfer function tests from color-tests.nix

--- systems/commit-messages/hetzner-commit-message.nix
+++ systems/commit-messages/hetzner-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "fix:_Update_sRGB_transfer_function_tests_with_precise_values________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "___________________________________________________________________________________wireguard";
+  system.nixos.label = "refactor:_Remove_sRGB_transfer_function_tests_from_color-tests.nix__________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "___________________________________________________________________________________wireguard";
 }
