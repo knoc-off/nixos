3149f33886f0e8dac76988f4e36133626ddb71cb
knoff <selby@niko.ink>
2025-04-09 23:25:00 +0200

    feat: Use color-lib to adjust lightness of bright kitty colors

--- systems/commit-messages/framework13-commit-message.nix
+++ systems/commit-messages/framework13-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "refactor:_Remove__prefix_from_color_values_in_theme.nix_____________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
+  system.nixos.label = "feat:_Use_color-lib_to_adjust_lightness_of_bright_kitty_colors______________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
 }
