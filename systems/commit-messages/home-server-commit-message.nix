8927c19e60ced4d06b939f352f3ae67c556356a9
knoff <selby@niko.ink>
2025-04-09 18:59:57 +0200

    fix: Update sRGB transfer function tests with precise values

--- systems/commit-messages/home-server-commit-message.nix
+++ systems/commit-messages/home-server-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "fix:_Corrected_input_value_for_srgb_transfer_function_inv_test______________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_____________________________________________________________________________big_restructure";
+  system.nixos.label = "fix:_Update_sRGB_transfer_function_tests_with_precise_values________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_____________________________________________________________________________big_restructure";
 }
