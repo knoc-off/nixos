98443b1f9cb52ed5dc1f7da872be2c9500a75690
knoff <selby@niko.ink>
2025-04-09 22:21:17 +0200

    refactor: Use color-lib and basic theme values in swayidle.nix

--- systems/commit-messages/raspberry-3b-commit-message.nix
+++ systems/commit-messages/raspberry-3b-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "refactor:_Use_basic_theme_values_and_rename_colorLib_to_color-lib___________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________minecraft_changes._disable_waydroid";
+  system.nixos.label = "refactor:_Use_color-lib_and_basic_theme_values_in_swayidle.nix______________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________minecraft_changes._disable_waydroid";
 }
