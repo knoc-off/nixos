42d050f250ac8b2cd95642d06a945c4d93d96750
knoff <selby@niko.ink>
2025-04-09 11:15:56 +0200

    refactor: Remove tracing from the exp function in math.nix

--- systems/commit-messages/framework13-commit-message.nix
+++ systems/commit-messages/framework13-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "feat:_Improve_math_functions_ln_exp_pow_sin_with_accuracy_and_tracing_______________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
+  system.nixos.label = "refactor:_Remove_tracing_from_the_exp_function_in_math.nix__________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
 }
