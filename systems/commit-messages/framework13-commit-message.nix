828fce723d2d03694a4baaf30239696b5e2775ba
knoff <selby@niko.ink>
2025-04-09 21:13:26 +0200

    refactor: Use color-lib functions for dunst theme configuration

--- systems/commit-messages/framework13-commit-message.nix
+++ systems/commit-messages/framework13-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "removed_old_color_conversion_code.__________________________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
+  system.nixos.label = "refactor:_Use_color-lib_functions_for_dunst_theme_configuration_____________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
 }
