354713547bb250d721f27e15c9eb8391a29ff067
knoff <selby@niko.ink>
2025-04-09 18:50:14 +0200

    Refactor: Improve error tracing for boundary checks in color tests

--- systems/commit-messages/framework13-commit-message.nix
+++ systems/commit-messages/framework13-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "fix:_Relax_boundary_check_tolerance_in_find_gamut_intersection_test_________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
+  system.nixos.label = "Refactor:_Improve_error_tracing_for_boundary_checks_in_color_tests__________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
 }
