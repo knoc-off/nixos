85a35c44408d5a640a4555254d67a4844aac8af2
knoff <selby@niko.ink>
2025-04-09 22:32:27 +0200

    refactor: Replace colorLib with color-lib in userChrome.nix

--- systems/commit-messages/home-server-commit-message.nix
+++ systems/commit-messages/home-server-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "refactor:_Use_theme_variables_directly_in_kitty_config______________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_____________________________________________________________________________big_restructure";
+  system.nixos.label = "refactor:_Replace_colorLib_with_color-lib_in_userChrome.nix_________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_____________________________________________________________________________big_restructure";
 }
