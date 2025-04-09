20b5e0646b56d92ab632b70250b78daf28825c51
knoff <selby@niko.ink>
2025-04-09 18:53:58 +0200

    fix: Correct hue calculation and handle grey axis in Okhsl/Okhsv

--- systems/commit-messages/framework13-commit-message.nix
+++ systems/commit-messages/framework13-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "fix:_Correct_toe_and_toe_inv_test_expectations_in_color-tests.nix___________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
+  system.nixos.label = "fix:_Correct_hue_calculation_and_handle_grey_axis_in_OkhslOkhsv_____________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________feat:_Update_aider_model_and_starship_prompt_add_postgresql";
 }
