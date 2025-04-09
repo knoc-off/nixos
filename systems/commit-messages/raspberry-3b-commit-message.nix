9faa5bcb94c74ce300c74dec2006181d5deadeb4
knoff <selby@niko.ink>
2025-04-09 11:16:15 +0200

    refactor: Remove tracing from exp function in math.nix

--- systems/commit-messages/raspberry-3b-commit-message.nix
+++ systems/commit-messages/raspberry-3b-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "refactor:_Remove_tracing_from_the_exp_function_in_math.nix__________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________minecraft_changes._disable_waydroid";
+  system.nixos.label = "refactor:_Remove_tracing_from_exp_function_in_math.nix______________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________minecraft_changes._disable_waydroid";
 }
