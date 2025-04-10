f395e082da308ed4fc385e9ed8fc87f83dbde1c7
knoff <selby@niko.ink>
2025-04-10 17:43:45 +0200

    feat: Generate accent colors using math.arange and setOkhslHue

--- systems/commit-messages/desktop-commit-message.nix
+++ systems/commit-messages/desktop-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "refactor:_Remove_Core_Palette_exposure_in_theme.nix_________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "______________________________feat:_Refactor_theme.nix_for_improved_color_palette_generation";
+  system.nixos.label = "feat:_Generate_accent_colors_using_math.arange_and_setOkhslHue______________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "______________________________feat:_Refactor_theme.nix_for_improved_color_palette_generation";
 }
--- systems/commit-messages/home-server-commit-message.nix
+++ systems/commit-messages/home-server-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "refactor:_Remove_Core_Palette_exposure_in_theme.nix_________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_____________________________________________________________________________big_restructure";
+  system.nixos.label = "feat:_Generate_accent_colors_using_math.arange_and_setOkhslHue______________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_____________________________________________________________________________big_restructure";
 }
