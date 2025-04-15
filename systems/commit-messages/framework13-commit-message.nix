0a4d8cfeb7aec8370b9e9e09556ec08266e5d0a0
knoff <selby@niko.ink>
2025-04-15 21:27:15 +0200

    fix: Correct orthographic projection and color constant usage

--- systems/commit-messages/framework13-commit-message.nix
+++ systems/commit-messages/framework13-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "fix:_Fix_bevy_example_address_deprecations_and_errors_______________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________________________________________gtk";
+  system.nixos.label = "fix:_Correct_orthographic_projection_and_color_constant_usage_______________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________________________________________gtk";
 }
