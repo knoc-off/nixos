b1778f81b6cd46341b6f6c0ac813c73792293de1
knoff <selby@niko.ink>
2025-04-16 14:11:48 +0200

    refactor: Simplify theme generation and adjust grayscale colors

--- systems/commit-messages/desktop-commit-message.nix
+++ systems/commit-messages/desktop-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "fix:_Add_missing_fields_to_OrthographicProjection_initializer_______________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "______________________________feat:_Refactor_theme.nix_for_improved_color_palette_generation";
+  system.nixos.label = "refactor:_Simplify_theme_generation_and_adjust_grayscale_colors_____________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "______________________________feat:_Refactor_theme.nix_for_improved_color_palette_generation";
 }
