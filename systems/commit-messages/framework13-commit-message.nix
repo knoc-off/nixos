e4cfacd0121dfcf52a304ce5ce282328570eba82
knoff <selby@niko.ink>
2025-04-16 14:12:04 +0200

    Refactor: Generate base colors using lightness interpolation

--- systems/commit-messages/framework13-commit-message.nix
+++ systems/commit-messages/framework13-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "refactor:_Simplify_theme_generation_and_adjust_grayscale_colors_____________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________________________________________gtk";
+  system.nixos.label = "Refactor:_Generate_base_colors_using_lightness_interpolation________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________________________________________gtk";
 }
