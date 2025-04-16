3c3a1c9262d90aa21f771dbe2f9438407cad3182
knoff <selby@niko.ink>
2025-04-16 14:14:09 +0200

    refactor: Improve grayscale color generation in theme.nix

--- systems/commit-messages/desktop-commit-message.nix
+++ systems/commit-messages/desktop-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "Refactor:_Generate_base_colors_using_lightness_interpolation________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "______________________________feat:_Refactor_theme.nix_for_improved_color_palette_generation";
+  system.nixos.label = "refactor:_Improve_grayscale_color_generation_in_theme.nix___________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "______________________________feat:_Refactor_theme.nix_for_improved_color_palette_generation";
 }
--- systems/commit-messages/hetzner-commit-message.nix
+++ systems/commit-messages/hetzner-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "Refactor:_Generate_base_colors_using_lightness_interpolation________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "___________________________________________________________________________________wireguard";
+  system.nixos.label = "refactor:_Improve_grayscale_color_generation_in_theme.nix___________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "___________________________________________________________________________________wireguard";
 }
