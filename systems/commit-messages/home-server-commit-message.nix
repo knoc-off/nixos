445f83417ef937bbfe761339b5febc697014d3bc
knoff <selby@niko.ink>
2025-04-17 17:37:15 +0200

    feat: Apply hue exponent curve conditionally based on threshold.

--- systems/commit-messages/home-server-commit-message.nix
+++ systems/commit-messages/home-server-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "chore:_Adjust_default_hue_offset_and_exponent_for_theme_colors______________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_____________________________________________________________________________big_restructure";
+  system.nixos.label = "feat:_Apply_hue_exponent_curve_conditionally_based_on_threshold.____________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_____________________________________________________________________________big_restructure";
 }
