de27dc90666bb96b9dd241208af982f990f1122e
knoff <selby@niko.ink>
2025-04-21 17:18:43 +0200

    refactor: Use lib.mkDefault to allow overriding hyprland settings

--- systems/commit-messages/raspberry-3b-commit-message.nix
+++ systems/commit-messages/raspberry-3b-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "refactor:_Use_lib_in_hyprland_module________________________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________minecraft_changes._disable_waydroid";
+  system.nixos.label = "refactor:_Use_lib.mkDefault_to_allow_overriding_hyprland_settings___________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________minecraft_changes._disable_waydroid";
 }
