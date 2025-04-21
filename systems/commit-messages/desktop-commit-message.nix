7d0d5c150f01a2e8e233a7444ea335114e088d9c
knoff <selby@niko.ink>
2025-04-21 16:16:14 +0200

    feat: Update grayscale generation to use linear interpolation.

--- systems/commit-messages/desktop-commit-message.nix
+++ systems/commit-messages/desktop-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "docs:_Add_comments_to_base_colors_in_theme.nix______________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "______________________________feat:_Refactor_theme.nix_for_improved_color_palette_generation";
+  system.nixos.label = "feat:_Update_grayscale_generation_to_use_linear_interpolation.______________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "______________________________feat:_Refactor_theme.nix_for_improved_color_palette_generation";
 }
