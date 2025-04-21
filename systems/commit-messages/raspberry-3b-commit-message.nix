a75130c5410647cdc52fa9e337f1c77d9b1ef47c
knoff <selby@niko.ink>
2025-04-21 11:51:55 +0200

    feat: Implement Bézier curves for lightness and hue in theme.nix

--- systems/commit-messages/raspberry-3b-commit-message.nix
+++ systems/commit-messages/raspberry-3b-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "feat:_Adjust_accent_colors_and_hue_offset_for_theme_generation______________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________minecraft_changes._disable_waydroid";
+  system.nixos.label = "feat:_Implement_Bzier_curves_for_lightness_and_hue_in_theme.nix_____________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________minecraft_changes._disable_waydroid";
 }
