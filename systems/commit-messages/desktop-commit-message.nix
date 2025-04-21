fdcf57129023404ed76e7c6ed2bcbe1fb1715cbc
knoff <selby@niko.ink>
2025-04-21 11:51:55 +0200

    feat: Implement Bézier curves for lightness and hue in theme.nix

--- systems/commit-messages/desktop-commit-message.nix
+++ systems/commit-messages/desktop-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "feat:_Adjust_accent_colors_and_hue_offset_for_theme_generation______________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "______________________________feat:_Refactor_theme.nix_for_improved_color_palette_generation";
+  system.nixos.label = "feat:_Implement_Bzier_curves_for_lightness_and_hue_in_theme.nix_____________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "______________________________feat:_Refactor_theme.nix_for_improved_color_palette_generation";
 }
--- systems/commit-messages/framework13-commit-message.nix
+++ systems/commit-messages/framework13-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "feat:_Adjust_accent_colors_and_hue_offset_for_theme_generation______________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________________________________________gtk";
+  system.nixos.label = "feat:_Implement_Bzier_curves_for_lightness_and_hue_in_theme.nix_____________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________________________________________gtk";
 }
