fceb570987a9755812441bbc5d6d1a44c6d5cbb6
knoff <selby@niko.ink>
2025-04-21 12:25:39 +0200

    refactor: Use standard cubicBezier in theme generation

--- systems/commit-messages/desktop-commit-message.nix
+++ systems/commit-messages/desktop-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "fix:_Pass_extra_arguments_to_theme_function_________________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "______________________________feat:_Refactor_theme.nix_for_improved_color_palette_generation";
+  system.nixos.label = "refactor:_Use_standard_cubicBezier_in_theme_generation______________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "______________________________feat:_Refactor_theme.nix_for_improved_color_palette_generation";
 }
