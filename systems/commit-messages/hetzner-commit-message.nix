a297342306be5e5b4956e8269fa2a50ba4761614
knoff <selby@niko.ink>
2025-04-21 16:07:50 +0200

    fix: Correct syntax for assertions in linearInterpolatePoints function

--- systems/commit-messages/hetzner-commit-message.nix
+++ systems/commit-messages/hetzner-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "feat:_Add_linear_interpolation_function_for_a_set_of_points_________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "___________________________________________________________________________________wireguard";
+  system.nixos.label = "fix:_Correct_syntax_for_assertions_in_linearInterpolatePoints_function______________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "___________________________________________________________________________________wireguard";
 }
