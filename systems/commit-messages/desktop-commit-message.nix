7cc8aa8d8c625f8039c197a45c625a913743cefd
knoff <selby@niko.ink>
2025-04-21 11:51:54 +0200

    feat: Adjust accent colors and hue offset for theme generation

--- systems/commit-messages/desktop-commit-message.nix
+++ systems/commit-messages/desktop-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "refactor:_Improve_theming_and_colorscheme_configurations____________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "______________________________feat:_Refactor_theme.nix_for_improved_color_palette_generation";
+  system.nixos.label = "feat:_Adjust_accent_colors_and_hue_offset_for_theme_generation______________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "______________________________feat:_Refactor_theme.nix_for_improved_color_palette_generation";
 }
