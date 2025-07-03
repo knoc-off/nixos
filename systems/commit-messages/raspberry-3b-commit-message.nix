285bd9b44cebac5666dfbe0a839cd1534e8039f0
knoff <selby@niko.ink>
2025-07-03 07:42:05 +0200

    mkSystem / nixosSystem - for darwin

--- systems/commit-messages/raspberry-3b-commit-message.nix
+++ systems/commit-messages/raspberry-3b-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "macneovim___________________________________________________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________minecraft_changes._disable_waydroid";
+  system.nixos.label = "mkSystem__nixosSystem_-_for_darwin__________________________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_________________________________________________________minecraft_changes._disable_waydroid";
 }
