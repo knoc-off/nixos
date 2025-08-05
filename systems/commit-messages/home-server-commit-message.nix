2a787286173e4709fb7acf5293067cc09fafdaa7
knoff <selby@niko.ink>
2025-08-05 20:51:10 +0200

    hyprland

--- systems/commit-messages/home-server-commit-message.nix
+++ systems/commit-messages/home-server-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "quick_neovim_push___________________________________________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_____________________________________________________________________________big_restructure";
+  system.nixos.label = "hyprland____________________________________________________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "_____________________________________________________________________________big_restructure";
 }
