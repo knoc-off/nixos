11ec6aa6af3f749b278a0de3fe4e9eed192e07d3
knoff <selby@niko.ink>
2025-09-21 13:52:11 +0200

    idk

--- systems/commit-messages/hetzner-commit-message.nix
+++ systems/commit-messages/hetzner-commit-message.nix
@@ -1,4 +1,4 @@
 { self, ... }:
 {
-  system.nixos.label = "small_improvements__________________________________________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "____________________________________________________astal_experiments._plus_website_changes.";
+  system.nixos.label = "idk_________________________________________________________________________________________" + "REV_" + toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown") + "____________________________________________________astal_experiments._plus_website_changes.";
 }
